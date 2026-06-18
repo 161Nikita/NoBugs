f0ce5155864801bdeb726457229747919cf03f04
161Nikita <nikita,160192@yandex.ru>
2026-06-18 15:19:43 +0300

    Homework-10 Практика. Решение проблем - В DebugTask8 изменил название переменной с -> epsilon и поменял ей значение на 1e-9 - В DebugTask4 поменял метод, добавил обработку null - DebugTask9 поменял оператор сравнения строк

--- src/main/java/homework_10/DebugTask4.java
+++ src/main/java/homework_10/DebugTask4.java
@@ -2,9 +2,13 @@ package homework_10;
 
 public class DebugTask4 {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("null"));
+        System.out.println(isPalindrome(null));
     }
     public static boolean isPalindrome(String str) {
+        if (str == null) {
+           System.out.println("null не может быть обработан, введите строку");
+           return false;
+        }
         String reversed = new StringBuilder(str).reverse().toString();
         return str.equals(reversed);
     }
--- src/main/java/homework_10/DebugTask8.java
+++ src/main/java/homework_10/DebugTask8.java
@@ -4,8 +4,8 @@ public class DebugTask8 {
     public static void main(String[] args) {
         double a = 0.1 * 3;
         double b = 0.3;
-        double c = 0.001;
-        if (Math.abs(a - b) < c) {
+        double epsilon  = 1e-9;
+        if (Math.abs(a - b) < epsilon) {
             System.out.println("Equal");
         } else {
             System.out.println("Not Equal");
